880a71559b3280a1e5a7b6d44849dec5f288277b
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-01-08 05:21:33 +0100

    Initial file setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className=" "></div>;
+}
+
+export default App;
